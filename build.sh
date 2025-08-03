e5dc20d338d19ba2723cfb76ee8be3cd60c2b52a
Placide <placidelinkpehoun@gmail.com>
2025-08-03 15:13:32 +0100

    render1

--- build.sh
+++ build.sh
@@ -2,7 +2,15 @@
 # exit on error
 set -o errexit
 
-pip install -r requirements.txt
+# Upgrade pip first
+pip install --upgrade pip
+
+# Install requirements with fallback for psycopg2
+pip install -r requirements.txt || {
+    echo "Failed to install psycopg2-binary, trying psycopg2..."
+    pip install psycopg2==2.9.9
+    pip install -r requirements.txt
+}
 
 python manage.py collectstatic --no-input
 python manage.py migrate 
